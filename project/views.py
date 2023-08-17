8d5c676c0970ff704b726b8abc21b355b1bb3247
Alash Shrestha <np03cs4s220081@heraldcollege.edu.np>
2023-08-17 19:01:52 +0545

    Updated Views

--- project/views.py
+++ project/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, "files/index.html")
 
 
 def resume(request):
-    return render(request, "resume.html")
+    return render(request, "files/resume.html")
 
 
 def projects(request):
-    return render(request, "projects.html")
+    return render(request, "files/projects.html")
 
 
 def contact(request):
-    return render(request, "contact.html")
+    return render(request, "files/contact.html")
