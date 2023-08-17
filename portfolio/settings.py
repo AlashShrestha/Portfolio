78d43006c37c56cc3d24007357a6608b0a26bb94
Alash Shrestha <np03cs4s220081@heraldcollege.edu.np>
2023-08-17 19:15:06 +0545

    Debug=True

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["www.alashshrestha.com.np", "portfolio-2ek8.onrender.com"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
